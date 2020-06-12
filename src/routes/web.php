9d8b9ec417f14187a9bb4bb7e8ed5a3d75c9cc1a
unknown <thiago@nortrix.com>
2020-06-12 19:47:39 -0300

    t2

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
+Route::get('/test', function (Request $request) {
 
     /*
     $users = DB::select('select * from users where id = ?', [1]);
