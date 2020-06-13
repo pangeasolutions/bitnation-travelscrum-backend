f747a06d7f60daa44fd34938686fb5e9833e8196
unknown <thiago@nortrix.com>
2020-06-13 11:06:42 -0300

    passport 9

--- src/routes/web.php
+++ src/routes/web.php
@@ -94,9 +94,6 @@ Route::get('/blockchain/passport/add', function () {
 
         $passport = DB::select('SELECT * FROM blockchain_passports ORDER BY id DESC LIMIT 1 ');
 
-        // var_dump($passport);
-
-
         return [
 
             "error" => "false",
@@ -125,12 +122,12 @@ Route::get('/blockchain/passport/read/{id}', function ($id) {
 
     $passport = DB::select('select * FROM blockchain_passports WHERE id = ? ', [$id]);
 
-    if(empty($id["id"])){
+    if(empty($passport[0]->id)){
 
         return [
 
             "error" => "true",
-            "error_message" => "Passport not found with id:".$passport["id"]
+            "error_message" => "Passport not found with id:".$id
 
         ];
 
@@ -141,9 +138,9 @@ Route::get('/blockchain/passport/read/{id}', function ($id) {
         "error" => "false",
         "error_message" => "",
         "passport_requested" => $id,
-        "file_name" => $passport["file_name"],
-        "file_hash" => $passport["file_hash"],
-        "block_hash" => $passport["block_hash"],
+        "file_name" => $passport[0]->file_name,
+        "file_hash" => $passport[0]->file_hash,
+        "block_hash" => $passport[0]->block_hash
 
     ];
 
